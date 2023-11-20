82686ec529d2c55ccabbd73572cd9ba6759e59db
Tovid Dfrei <149096220+toviddfrei@users.noreply.github.com>
2023-11-20 23:48:41 +0100

    Update dvc.js

--- dvc.js
+++ dvc.js
@@ -17,3 +17,24 @@ str.includes('quick'); // true
 str.endsWith('dog'); // true
 
 str.startsWith('Foo'); // false
+
+str.repeat(5); // "The quick brown fox jumped over the lazy dogThe quick brown fox jumped over the lazy dogThe quick brown fox jumped over the lazy dogThe quick brown fox jumped over the lazy dogThe quick brown fox jumped over the lazy dog"
+
+str.match(/((\(\d{3}\) ?)|(\d{3}-))?\d{3}-\d{4}/) // null
+
+'555-555-5555'.match(/((\(\d{3}\) ?)|(\d{3}-))?\d{3}-\d{4}/) // (4) ["555-555-5555", "555-", undefined, "555-", index: 0, input: "555-555-5555"]0: "555-555-5555"1: "555-"2: undefined3: "555-"index: 0input: "555-555-5555"length: 4__proto__: Array(0)
+
+str.replace('fox', 'wolf'); // "The quick brown wolf jumped over the lazy dog"
+
+'555-555-5555 is my phone number'.search(/((\(\d{3}\) ?)|(\d{3}-))?\d{3}-\d{4}/) // 0
+
+'Hi, 555-555-5555 is my phone number'.search(/((\(\d{3}\) ?)|(\d{3}-))?\d{3}-\d{4}/) // 4
+
+str.indexOf('jumped'); // 20
+
+str.lastIndexOf('jumped'); // 20
+
+var str2 = str.concat('again and again');
+
+str2.indexOf('again'); // 44
+str2.lastIndexOf('again'); // 54
